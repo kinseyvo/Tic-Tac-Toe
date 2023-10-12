a8df1d84ba66ab284b8725037e20bdd7445fba5b
kinseyvo <kinseyvo@csu.fullerton.edu>
2023-10-12 16:46:14 -0700

    rotation test

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+//import App from './App';
+//import App from './App-Lec12-Ex1-api-1a'
+//import App from './App-Lec12-Ex2-responsive'
+import App from './App-Lec12-Ex2-responsive-1a'
+//import App from './App-Lec12-Ex3-touch.js'
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
